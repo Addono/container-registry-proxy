0fd62a02cd5ba83628c5a5910f5204af472b8866
Adriaan Knapen <hi@aknapen.nl>
2020-05-07 13:13:19 +0200

    refactor(proxy): improve redirecting behaviour

--- src/index.ts
+++ src/index.ts
@@ -42,12 +42,12 @@ const server = http.createServer((req, res) => {
     ? `${REGISTRY_HOST}/${version}/${parameters.repository}/${Method[parameters.method]}/${parameters.tag}`
     : `${REGISTRY_HOST}/${version}/`
 
+  // Pause the ongoing request until the forwarded request returns
+  req.pause()
+
   console.log(`==> Forwarding request to ${url}\n`)
 
   console.log('\t-> ', req.headers)
-
-  // Pause the ongoing request until the forwarded request returns
-  req.pause()
 
   const connection = (HTTPS ? https : http).request(
     `${HTTPS ? 'https' : 'http'}://${url}`,
@@ -67,23 +67,40 @@ const server = http.createServer((req, res) => {
 
       serverResponse.pause()
 
-      // fix host and pass through.
-      if (statusCode && [301, 302, 303].includes(statusCode)) {
-        statusCode = 303
-        headers['location'] = `http://localhost:${PORT}/${headers['location']}`
-
-        console.log('\t-> Redirecting to ', headers['location'])
-
-        res.writeHead(statusCode, headers)
-        serverResponse.pipe(res)
-      } else if (statusCode && 200 <= statusCode && statusCode < 500) {
-        res.writeHead(statusCode, headers)
-        serverResponse.pipe(res)
-      } else {
+      // Return an error if the error code is not within [200, 500)
+      if (!statusCode || statusCode < 200 || statusCode >= 500) {
         const stringifiedHeaders = JSON.stringify(headers, null, 4)
         return failRequest(`Error ${statusCode}\n${stringifiedHeaders}`)
       }
 
+      // fix host and pass through.
+      if (statusCode && [301, 302, 303].includes(statusCode)) {
+        const location = headers['location']
+
+        /*
+         * Group 1: Protocol
+         * Group 2: Hostname
+         * Group 3: Path
+         */
+        const REDIRECT_LOCATION_MATCHER = /^(https?:\/\/)([^\/]*)([\s\S]*)$/
+        const redirectLocationMatches = location?.match(REDIRECT_LOCATION_MATCHER)
+        if (!redirectLocationMatches) {
+          return failRequest(
+            `Received redirect response code ${statusCode} however no redirect location detected: ${location}.`
+          )
+        }
+
+        const [, protocol, domain, path]: string[] = redirectLocationMatches
+        headers['location'] = protocol + domain.replace(REGISTRY_HOST, `localhost:${PORT}`) + path
+
+        statusCode = 303
+        console.log('\t-> Redirecting to ', headers['location'])
+      }
+
+      res.writeHead(statusCode, headers)
+
+      // Forward the request to the proxy
+      serverResponse.pipe(res)
       serverResponse.resume()
 
       console.log('\n\n')
