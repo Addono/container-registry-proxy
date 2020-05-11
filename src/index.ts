e7d69bc54c8cd9266d35037c810e79040ad43858
Adriaan Knapen <hi@aknapen.nl>
2020-05-11 21:40:20 +0200

    refactor(proxy): break out proxy logic into a separate class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import http from 'http'
-import https from 'https'
-import proxyUrlParser from './proxyUrlParser'
-import { loadPlugins, Method } from './plugins'
-import tagChaos from './plugins/tagChaos'
+import { Plugin, loadPlugins } from './plugins'
+import proxyServer from './proxyServer'
 
-const PORT: string = process.env?.PORT ?? '8080'
+export const PORT: string = process.env?.PORT ?? '8080'
 export const REGISTRY_HOST: string = process.env?.REGISTRY_HOST ?? 'registry.hub.docker.com'
 export const HTTPS: boolean = process.env?.HTTPS?.toLowerCase() != 'false' ?? true
 
@@ -18,105 +15,9 @@ const settingsFormatted: string = [
 
 console.log(`Configuration:\n${settingsFormatted}\n`)
 
-const { requestPipe } = loadPlugins()
+const plugins: Plugin = loadPlugins()
 
-const server = http.createServer(async (req, res) => {
-  const failRequest = (message: string) => {
-    console.error(message)
-
-    res.writeHead(500, { 'content-type': 'text/plain' }).end(message)
-  }
-
-  console.log('Received request for ', req.url)
-
-  if (!req.url) {
-    return failRequest('Path cannot be empty')
-  }
-
-  const containerRegistryRequest = proxyUrlParser(req.url)
-  if (!containerRegistryRequest) {
-    return failRequest(`Could not parse url "${req.url}"`)
-  }
-
-  const requestPostPipe = await requestPipe(containerRegistryRequest)
-  if (!requestPostPipe) {
-    return failRequest(`Request aborted by plugins`)
-  }
-
-  const { version, parameters } = requestPostPipe
-
-  const url = parameters
-    ? `${REGISTRY_HOST}/${version}/${parameters.repository}/${Method[parameters.method]}/${parameters.tag}`
-    : `${REGISTRY_HOST}/${version}/`
-
-  // Pause the ongoing request until the forwarded request returns
-  req.pause()
-
-  console.log(`==> Forwarding request to ${url}\n`)
-
-  // console.log('\t-> ', req.headers)
-
-  const connection = (HTTPS ? https : http).request(
-    `${HTTPS ? 'https' : 'http'}://${url}`,
-    {
-      headers: {
-        ...req.headers,
-        host: REGISTRY_HOST, // Overwrite the host as the prevent certificate issues
-      },
-      method: req.method,
-      agent: false,
-    },
-    (serverResponse) => {
-      let { statusCode, headers } = serverResponse
-
-      console.log('<== Received response for', statusCode, url)
-      // console.log('\t-> Response Headers: ', headers)
-
-      serverResponse.pause()
-
-      // Return an error if the error code is not within [200, 500)
-      if (!statusCode || statusCode < 200 || statusCode >= 500) {
-        const stringifiedHeaders = JSON.stringify(headers, null, 4)
-        return failRequest(`Error ${statusCode}\n${stringifiedHeaders}`)
-      }
-
-      // fix host and pass through.
-      if (statusCode && [301, 302, 303].includes(statusCode)) {
-        const location = headers['location']
-
-        /*
-         * Group 1: Protocol
-         * Group 2: Hostname
-         * Group 3: Path
-         */
-        const REDIRECT_LOCATION_MATCHER = /^(https?:\/\/)([^\/]*)([\s\S]*)$/
-        const redirectLocationMatches = location?.match(REDIRECT_LOCATION_MATCHER)
-        if (!redirectLocationMatches) {
-          return failRequest(
-            `Received redirect response code ${statusCode} however no redirect location detected: ${location}.`
-          )
-        }
-
-        const [, protocol, domain, path]: string[] = redirectLocationMatches
-        headers['location'] = protocol + domain.replace(REGISTRY_HOST, `localhost:${PORT}`) + path
-
-        statusCode = 303
-        console.log('\t-> Redirecting to ', headers['location'])
-      }
-
-      res.writeHead(statusCode, headers)
-
-      // Forward the request to the proxy
-      serverResponse.pipe(res)
-      serverResponse.resume()
-
-      console.log('\n\n')
-    }
-  )
-
-  req.pipe(connection)
-  req.resume()
-})
+const server = proxyServer(plugins)
 
 console.log('Starting server...\n')
 
