dbc27592e4a1faddf953111e088cbb302d7e8081
Adriaan Knapen <hi@aknapen.nl>
2020-05-14 16:09:29 +0200

    fix(requests): retrieve more tags when requesting them from the registry
    Now it first tries to request the tags using the v1 API, which returns all entries. v2 is still present as a fallback, for which the page size is increased from undefined (defaults to 10) to 10000. This will be capped at the server side to 100 by Docker Hub.

--- src/requests/getTags.ts
+++ src/requests/getTags.ts
@@ -9,15 +9,37 @@ const getTags: (opts: { repository: string; https: boolean; host: string }) => P
   https,
   host,
 }) => {
+  // Prefer the v1 API over v2, as it will return all tags on the first request
   try {
-    const result = await request({ https, host, path: `/v2/repositories/${repository}/tags/` })
-    const tags = result.data?.results?.map(({ name }: { name: string }) => ({ name }))
+    const result = await request({ https, host, path: `/v1/repositories/${repository}/tags` })
+    const tags = result.data?.map(({ name }: { name: string }) => ({ name }))
 
-    return tags ?? []
+    if (tags?.length > 0) {
+      return tags
+    }
   } catch (error) {
     console.error(error)
-    return []
   }
+
+  console.log('Failed retrieving tags using the v1 API, falling back to v2')
+
+  try {
+    // @todo Add pagination to properly iterate through all tags
+    const result = await request({
+      https,
+      host,
+      path: `/v2/repositories/${repository}/tags/?page_size=10000`,
+    })
+    const tags = result.data?.results?.map(({ name }: { name: string }) => ({ name }))
+
+    if (tags?.length > 0) {
+      return tags
+    }
+  } catch (error) {
+    console.error(error)
+  }
+
+  return []
 }
 
 export default getTags
